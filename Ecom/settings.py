edfc054c5e8179b5d74be914d424b4039f697b2a
anmolsingla-webdev <f20200633@pilani.bits-pilani.ac.in>
2021-06-09 12:39:45 +0530

    dochange

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i-xg)c4$26ld6)1dy38^mzzex5cap&^57d8j3n8hlbn@1dfc!%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['165.232.177.28']
+ALLOWED_HOSTS = ['www.ecom.games','165.232.177.28']
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'become_admin'
 LOGOUT_REDIRECT_URL = 'home'
