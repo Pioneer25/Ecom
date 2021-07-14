15a4c3a444f3695b35c262c2db21f93d50940900
anmolsingla-webdev <f20200633@pilani.bits-pilani.ac.in>
2021-07-14 14:43:41 +0530

    letchange1

--- product/models.py
+++ product/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=255)
     
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.PositiveIntegerField
     date_added = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
