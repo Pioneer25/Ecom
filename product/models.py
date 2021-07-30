6d77cc7f0d11412dfb9ea6cb49545be9c3f6111d
anmolsingla-webdev <f20200633@pilani.bits-pilani.ac.in>
2021-07-30 16:12:47 +0530

    letchange10

--- product/models.py
+++ product/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     date_added = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=False, null=False)
     users_wishlist = models.ManyToManyField(User, related_name="user_wishlist", blank=True)
 
     class Meta:
@@ -29,27 +29,4 @@ class Product(models.Model):
         return self.title
     
     
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return self.thumbnail.url
-        else:
-            if self.image:
-                self.thumbnail = self.make_thumbnail(self.image)
-                self.save()
-
-                return self.thumbnail.url
-            else:
-                return 'https://via.placeholder.com/240x180.jpg'
-    
-    def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
-        img.convert('RGB')
-        img.thumbnail(size)
-
-        thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
-
-        thumbnail = File(thumb_io, name=image.name)
-
-        return thumbnail
    
