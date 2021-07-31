94b7494b2ae4b49e66d12899059aae738958a49e
anmolsingla-webdev <f20200633@pilani.bits-pilani.ac.in>
2021-07-31 15:11:33 +0530

    Newchange

--- customer/models.py
+++ customer/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.OneToOneField(User, related_name='customer', on_delete=models.CASCADE)
-    Email = models.EmailField(max_length = 200)
+    Email = models.EmailField(max_length = 200, null=True)
 
     class Meta:
         ordering = ['created_at']
