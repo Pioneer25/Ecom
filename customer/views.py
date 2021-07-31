0a864e81821270080373d29c21d1768f0acc9f22
anmolsingla-webdev <f20200633@pilani.bits-pilani.ac.in>
2021-07-31 22:34:46 +0530

    Newchange1

--- customer/views.py
+++ customer/views.py
@@ -23,11 +23,12 @@ def become_customer(request,backend='django.contrib.auth.backends.ModelBackend')
 
             customer = Customer.objects.create(name=user.username, created_by=user)
 
-          return redirect('home')
-      else:
+            return redirect('home')
+    else:
         form = UserCreationForm()
 
     return render(request, 'customer/become_customer.html', {'form': form})
+
 
 @login_required
 def customer_admin(request):
